c0a6e4360444df593e32837e3d70013c2126526c
Viswavardinii <viswa04104@gmail.com>
2024-11-08 08:31:20 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,6 @@
+public class Hello{
+  public static void main(String args[]){
+    System.out.println("Hi from Jenkins.");
+    System.out.println("Jenkins implemented successfully.");      
+  }
+}
